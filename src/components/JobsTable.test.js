1132bd93a77fd3ed10fa285f69e847d0b90cd42c
Yemi Fakorede <yemifakorede@gmail.com>
2021-11-10 20:53:40 -0500

    cleaned up imports

--- src/components/JobsTable.test.js
+++ src/components/JobsTable.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { TableBody, TableRow } from "./JobsTable";
 
 test("table body loading data", async () => {
